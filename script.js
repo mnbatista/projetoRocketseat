a290e4de14772bec5bbf73de565dab234ae93c2f
Maycon de Novaes Batista <maycon_novaes@hotmail.com>
2023-08-23 22:53:47 -0300

    Ajuste na url da imgem

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function toggleMode() {
-  const html = document.documentElement;
-  html.classList.toggle("light");
-  const img = document.querySelector("#profile img");
+  const html = document.documentElement
+  html.classList.toggle("light")
+  const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "../assets/avatar-light.png");
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    img.setAttribute("src", "../assets/avatar.png");
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
